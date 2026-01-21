6e0b072d7f372019ee8c853a9d1e7fedbb1e130b
user <613410089@alum.ccu.edu.tw>
2026-01-21 12:37:00 +0000

    fix Reset() cause program hangs on arithmetic protocol

--- src/motioncore/base/register.cpp
+++ src/motioncore/base/register.cpp
@@ -136,7 +136,8 @@ void Register::Reset() {
   evaluated_gates_online_ = 0;
   gates_setup_done_flag_ = false;
   gates_online_done_flag_ = false;
-  
+  gates_online_ = 0;
+  gates_setup_ = 0;
   
 }
 
